93007356db8608dd7b7e6e154ff5952f29d4c195
IonutInit <ionutcojocaru@outlook.com>
2022-10-02 12:59:39 +0100

    latest version of RPS

--- 03. Rock Paper Scissors (day 5 and counting)/main.js
+++ 03. Rock Paper Scissors (day 5 and counting)/main.js
@@ -1,5 +1,3 @@
-
-//defining game score variables
 let userWins = 0;
 let computerWins = 0;
 let draws  = 0;
@@ -11,93 +9,101 @@ let userFinalInput;
 let computerFinalInput;
 let keepOnPlaying;
 
+let gameRatio = userWins/computerWins;
+let gameRigging = 0.5;
+let gameCounter = 0;
 
 
-
-//user input
-function userAnswer() {  
-    userAnswer = prompt("Rock (R), Paper(P), Scissors(S)?");
-    let userGame = userAnswer.toUpperCase();
-    if (userGame === "R") {
-        return userGame = "Rock"
-    } else if  (userGame === "P") {
-        return userGame = "Paper"
-    } else if (userGame === "S") {
-        return userGame = "Scissors"
-    }
-}
-
-userFinalInput = userAnswer(userGame);
-
-
-
-//computer choice
-
-function computerAnswer() {
-    let randomChoice = Math.floor(Math.random ()*3);
-let computerGame
-if (randomChoice === 0) {
-        return computerGame = "Rock"
-    }
-    else if (randomChoice === 1) {
-        return computerGame = "Paper"
-    }
-    else if (randomChoice === 2) {
-        return computerGame = "Scissors"
-    }
-}
-
-computerFinalInput = computerAnswer(computerGame)
-
-
-//coding the game 
-function game() {
-    if (userFinalInput == "Rock") {
-        if (computerFinalInput == "Rock") {
-            draws++;
-            result = "It's a draw!"
-        } else if (computerFinalInput == "Scissors") {
-            computerWins++;
-            result = "Boo! Computer wins..."
-        } else {
-           userWins++;
-           result = "You win!"
-        }
-    }  else if (userFinalInput == "Scissors") {
-        if (computerFinalInput == "Scissors") {
-            draws++;
-            result = "It's a draw!"
-        } else if (computerFinalInput == "Rock") {
-            computerWins++;
-            result = "Boo! Computer wins..."
-        } else {
-            userWins++;
-            result = "You win!"
-        }
-    }  else if (userFinalInput == "Paper") {
-        if (computerFinalInput == "Paper") {
-            draws++;
-            result = "It's a draw!"
-        } else if (computerFinalInput == "Scissors") {
-            computerWins++;
-            result = "Boo! Computer wins..."
-        } else {
-            userWins++;
-            result = "You win!"
+while (confirm("Continue?")) {
+    function userAnswer() {  
+        userAnswer = prompt("Rock (R), Paper(P), Scissors(S)?");
+        userGame = userAnswer.toUpperCase();
+        if (userGame === "R") {
+            return userGame = "Rock"
+        } else if  (userGame === "P") {
+            return userGame = "Paper"
+        } else if (userGame === "S") {
+            return userGame = "Scissors"
         }
     }
-    alert(`You chose ${userFinalInput}. Computer chose ${computerFinalInput}. ${result}.  The score is ${userWins} - ${computerWins} (${draws} draws).`);
-    document.getElementById("playerChoice").innerHTML = `You chose ${userFinalInput}.`;
-    document.getElementById("computerChoice").innerHTML = `Computer chose ${computerFinalInput}.`;
-    keepOnPlaying = confirm("Continue?");
+    
+    userFinalInput = userAnswer(userGame);
+        
+    
+    //computer choice
+    
+    function computerAnswer() {
+        let randomChoice = Math.floor(Math.random ()*3);
+        if (gameRatio > gameRigging && gameCounter > 1){
+    //the rigged game, if at least one game has been played
+    //with the current ratio of .5, it means that if user wins first game, computer automatically wins the second
+            if (userGame === "R") {
+                return computerGame === "Paper"
+            } else if (userGame === "P") {
+                return computerGame === "Scissors"
+            } else if (userGame === "S") {
+                return computerGame = "Rock"
+            }
+    //the correct game
+        } else {
+            if (randomChoice === 0) {
+                return computerGame = "Rock"
+            }
+            else if (randomChoice === 1) {
+                return computerGame = "Paper"
+            }
+            else if (randomChoice === 2) {
+                return computerGame = "Scissors"
+            }
+        }    
+    }
+    
+    computerFinalInput = computerAnswer(computerGame)
+       
+    
+    //coding the game 
+    function game(human, computer) {
+        if (human == "Rock") {
+            if (computer == "Rock") {
+                draws++;
+                result = "It's a draw!"
+            } else if (computer == "Scissors") {
+                computerWins++;
+                result = "Boo! Computer wins..."
+            } else {
+               userWins++;
+               result = "You win!"
+            }
+        }  else if (human == "Scissors") {
+            if (computer == "Scissors") {
+                draws++;
+                result = "It's a draw!"
+            } else if (computer == "Rock") {
+                computerWins++;
+                result = "Boo! Computer wins..."
+            } else {
+                userWins++;
+                result = "You win!"
+            }
+        }  else if (human == "Paper") {
+            if (computer == "Paper") {
+                draws++;
+                result = "It's a draw!"
+            } else if (computer == "Scissors") {
+                computerWins++;
+                result = "Boo! Computer wins..."
+            } else {
+                userWins++;
+                result = "You win!"
+            }
+        }
+        alert(`You chose ${userFinalInput}. Computer chose ${computerFinalInput}. ${result}.  The score is ${userWins} - ${computerWins} (${draws} draws).`);  
+    }
+    
+    gameCounter++;
+    game(userFinalInput, computerFinalInput);
 }
 
-game();
 
-
-
-//! I wasn't able to create a game function that accepts parameters; this is OK for playing once, but puting this in a loop just make it repeats itself!
-
-// while (keepOnPlaying) {
-//     game();
-// }
+//It kind of works, if you ignore the fact that it starts with the prompt, but I'm not happy about the method used, of having to nest everything
+//inside the loop (Excel style), instead of elegantly calling each thing when I needed it 
